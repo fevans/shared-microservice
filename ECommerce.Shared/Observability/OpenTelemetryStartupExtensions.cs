3127f24f5415806368e4767476255a56e9d0e1e5
Francis Evans <francis.evans@gmail.com>
2025-03-08 02:21:15 +0000

    Added OpenTelemetry packages and OpenTelemetryStartupExtensions.cs to configure basic tracing and observability

---
+++ ECommerce.Shared/Observability/OpenTelemetryStartupExtensions.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace ECommerce.Shared.Observability;
+
+public static class OpenTelemetryStartupExtensions
+{
+    public static OpenTelemetryBuilder AddOpenTelemetryTracing(this IServiceCollection services, string serviceName)
+    {
+        return services.AddOpenTelemetry()
+            .ConfigureResource(r => r
+                .AddService(serviceName))
+            .WithTracing(builder =>
+            {
+                builder
+                    .AddConsoleExporter()
+                    .AddAspNetCoreInstrumentation();
+            });
+    }
+}
